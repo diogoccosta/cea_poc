ff3d91ede021c50bdcf78ce087df6e6368821be4
Giovanni Degani <giovanni.degani@sap.com>
2015-05-26 14:34:10 -0300

    plugin network information added

--- platforms/android/assets/www/js/mapa.js
+++ platforms/android/assets/www/js/mapa.js
@@ -1,26 +1,26 @@
    var markers = [{
        name: "Shopping Eldorado",
-       address: "Av. Rebouças 3970",
+       address: "Av. Rebouças 3970, São Paulo",
        lat: -23.572337,
        lon: -46.695728
    }, {
        name: "Shopping Morumbi",
-       address: "Av. Roque Petroni Júnior 1089",
+       address: "Av. Roque Petroni Júnior 1089, São Paulo",
        lat: -23.623190,
        lon: -46.698922
    }, {
        name: "Centro de São Paulo",
-       address: "Rua 24 de Maio 70",
+       address: "Rua 24 de Maio 70, São Paulo",
        lat: -23.544111,
        lon: -46.639375
    }, {
        name: "Shopping Granja Vianna",
-       address: "Rod Raposo Tavares Km 23,5",
+       address: "Rodovia Raposo Tavares Km 23,5, Cotia",
        lat: -23.591397,
        lon: -46.833688
    }, {
        name: "Shopping Tamboré",
-       address: "Av Piracema 669",
+       address: "Av Piracema 669, São Paulo",
        lat: -23.504158,
        lon: -46.834677
    }];
@@ -45,74 +45,38 @@
            button.addEventListener("click", function() {
                map.showDialog();
            }, false);
+           var functionArray = [];
 
-           for (var i = 0; i < markers.length; i++) {
-               currentMarker = markers[i];
-               var coord = new plugin.google.maps.LatLng(currentMarker.lat, currentMarker.lon);
-               map.addMarker({
-                   'position': coord,
-                   'title': currentMarker.name,
-                   'snippet': currentMarker.address,
-                   'markerClick': function(marker) {
-                       marker.showInfoWindow();
-                   },
-                   'infoClick': function(marker) {
-                       console.log(marker);
+           var createFunction = function(i) {
+               return function() {
+                  console.log(i);
+                   map.addMarker({
+                       'position': coord,
+                       'title': currentMarker.name,
+                       'snippet': currentMarker.address,
+                       'markerClick': function(marker) {
+                           marker.showInfoWindow();
+                       },
+                       'infoClick': function() {
 
-                       var locationSuccess = function(location) {
-                           console.log("Location:");
-                           console.log(location);
                            launchnavigator.navigate(
-                               [currentMarker.lat, currentMarker.lon], null,
+                               [markers[i].lat, markers[i].lon], null,
                                function() {
                                    console.log("Plugin success");
                                },
                                function(error) {
                                    console.log("Plugin error: " + error);
                                });
-                           // var request = {
-                           //     'position': location.latLng
-                           // };
+                       }
+                   });
+               };
+           };
 
-                           //                            plugin.google.maps.Geocoder.geocode(request, function(results) {
-                           //                                if (results.length) {
-                           //                                    var result = results[0];
-                           //                                    var position = result.position;
-                           //                                    var address = [
-                           //                                        result.subThoroughfare || "",
-                           //                                        result.thoroughfare || "",
-                           //                                        result.locality || "",
-                           //                                        result.adminArea || "",
-                           //                                        result.postalCode || "",
-                           //                                        result.country || ""
-                           //                                    ].join(", ");
+           for (var i = 0; i < markers.length; i++) {
+               currentMarker = markers[i];
 
-                           //                                    var sourceAddress = "saddr=" + address;
-                           //                                    var destAddress = "daddr=" + currentMarker.address;
-
-                           // //                                   var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
-
-
-                           // //                                   var url = "";
-
-                           //                                    // if (deviceType == "Android") {
-                           //                                    //     url = "geo:?" + sourceAddress + "&" + destAddress;
-                           //                                    // } else if (deviceType == "iPad" || deviceType == "iPhone") {
-                           //                                    //     url = "maps://maps.apple.com/?" + sourceAddress + "&" + destAddress;
-                           //                                    // }                                  
-                           //                                    // window.open(url, '_system',"");
-
-                           //                                } else {
-                           //                                    console.log("Not found");
-                           //                                }
-
-
-                           // });
-                       };
-
-                       map.getMyLocation(locationSuccess, onError);
-                   }
-               });
+               var coord = new plugin.google.maps.LatLng(currentMarker.lat, currentMarker.lon);
+               createFunction(i)();
            }
 
            var onSuccess = function(location) {
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -92,6 +92,21 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
@@ -128,6 +143,7 @@ module.exports.metadata =
     "com.sap.mp.cordova.plugins.odata": "3.7.1",
     "plugin.google.maps": "1.2.5",
     "uk.co.workingedge.phonegap.plugin.LaunchNavigator": "2.6.0",
+    "org.apache.cordova.network-information": "0.2.15",
     "com.sap.mp.cordova.plugins.corelibs": "3.7.1",
     "org.apache.cordova.device": "0.3.0",
     "org.apache.cordova.inappbrowser": "0.3.4-patched",
